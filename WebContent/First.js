056f169265af1112f70f111f8689bbe6a9677a0c
KEN <373116246@qq.com>
2019-01-17 01:10:47 +0800

    test

---
+++ WebContent/First.js
@@ -0,0 +1,19 @@
+new Vue({
+	el:'#app',
+	data:{
+
+	lineshow3:false,
+     lineshow4:false,
+     lineshow5:false,
+     column3:'column3',
+     homeClass:'home',
+     column4:'column4',
+     infoClass:'info',
+     column5:'column5',
+     optClass:'opt',
+     isHome:false,
+     isInfo:false,
+     isOpt:false,
+    }
+	
+});
