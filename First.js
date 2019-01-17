0ef52a5a926f7d84812b5479c701501af8770c33
KEN <373116246@qq.com>
2019-01-17 11:45:29 +0800

    add img

--- First.js
+++ First.js
@@ -2,18 +2,18 @@ new Vue({
 	el:'#app',
 	data:{
 
-	lineshow3:false,
-     lineshow4:false,
-     lineshow5:false,
-     column3:'column3',
-     homeClass:'home',
-     column4:'column4',
-     infoClass:'info',
-     column5:'column5',
-     optClass:'opt',
-     isHome:false,
-     isInfo:false,
-     isOpt:false,
+         lineshow3:false,
+         lineshow4:false,
+         lineshow5:false,
+         column3:'column3',
+         homeClass:'home',
+         column4:'column4',
+         infoClass:'info',
+         column5:'column5',
+         optClass:'opt',
+         isHome:false,
+         isInfo:false,
+         isOpt:false,
     }
-	
+    
 });
